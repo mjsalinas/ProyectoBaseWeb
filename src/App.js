8ef6ed489f123f477aca12c21d3ebb1cdbaec63d
Maria Jose Salinas <mariasalinas713@gmail.com>
2025-04-26 17:46:26 -0600

    Primer commit con creacion de proyecto react implementando Parcel

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+
+    return (
+        <div>
+            <h1>Hola Mundo desde React</h1>
+            <p> Creando mi primera aplicacion desde React y utilizando el React DOM</p>
+        </div>
+    );
+}
+
+export default App;
