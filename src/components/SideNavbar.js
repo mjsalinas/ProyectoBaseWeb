48a068ec1d49d2b1e122df7cfe36e32a597d6c5e
Maria Jose Salinas <mariasalinas713@gmail.com>
2025-05-17 17:18:05 -0600

    creando pagina de Gestion de Libros (CRUD) con formulario que actualiza variable de estado y muestra informacion en una tabla

--- src/components/SideNavbar.js
+++ src/components/SideNavbar.js
@@ -6,9 +6,9 @@ function SideNavbar() {
     <div className="bg-dark position-fixed" style={{ top: '56px', width: '200px', height: '100%' }}>
       <ul className="nav flex-column mt-3">
         <li><Link to="/" className="nav-link text-white">Dashboard</Link></li>
-        <li><Link to="/perfil" className="nav-link text-white">Perfil</Link></li>
-        <li><Link to="/libros" className="nav-link text-white">Libros</Link></li>
-        <li><Link to="/registro" className="nav-link text-white">Registro</Link></li>
+        <li><Link to="/" className="nav-link text-white">Perfil</Link></li>
+        <li><Link to="/books" className="nav-link text-white">Libros</Link></li>
+        <li><Link to="/" className="nav-link text-white">Registro</Link></li>
       </ul>
     </div>
   );
---
+++ src/pages/masters/Books.js
@@ -0,0 +1,75 @@
+import { useState } from "react";
+
+function Books() {
+    const [editIndex, setEditIndex] = useState(null);
+    const [books, setBooks] = useState([]);
+    const [form, setForm] = useState({'title':'', 'author':'',
+        'description':'','publishDate':''});
+
+        const handleOnChange = (e) => {
+            setForm({...form,[e.target.name]: e.target.value});
+        }
+
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        if(editIndex !== null){
+            const updatedBooks = [...books];
+            updatedBooks[editIndex] = form;
+            setBooks(updatedBooks);
+            setEditIndex(null);
+        }else{
+            setBooks([...books, form]);
+        }
+        setForm({'title':'', 'author':'',
+            'description':'','publishDate':''});
+    }
+    return (
+        <div>
+            <h2>Gestion de Libros</h2>
+            <form onSubmit={handleOnSubmit} className="mb-4">
+                {['title', 'author','description','publishDate'].map(
+                    (campo)=> (
+                        <input 
+                            key={campo} 
+                            name={campo} 
+                            value={form[campo]} 
+                            onChange={handleOnChange}
+                            placeholder={campo}
+                            className="form-control mb-2"
+                        /> 
+                    )
+                )}
+                <button> {editIndex !== null ? 'Actualizar' : 'Agregar'} </button>
+            </form>
+            
+            <table  className="table"> 
+                <thead>
+                    <tr>
+                        <th>Titulo</th>
+                        <th>Autor</th>
+                        <th>Description</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {books.map((book, id)=>(
+                        <tr key={id}>
+                            <td> {book.title} </td>
+                            <td> {book.author} </td>
+                            <td> {book.description} </td>
+                            <td> 
+                                <button className="btn btn-sm btn-warning me-2" onClick={()=>{}}>Edit</button>
+                                <button className="btn btn-sm btn-danger" onClick={()=>{}}>Delete</button>
+                            </td>
+                        </tr>
+                       ))
+                    }
+                </tbody>
+            </table>
+
+
+        </div>
+    );
+}
+
+export default Books;
