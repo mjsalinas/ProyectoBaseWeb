a32088391406c74ce98f29ae335d66e0f0cc06bd
Maria Jose Salinas <mariasalinas713@gmail.com>
2025-06-09 20:14:19 -0600

    implementando login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
+import { loginUser } from '../api/authService';
+import { supabase } from '../utils/supabaseClient';
 
 function Login() {
   const [email, setEmail] = useState('');
@@ -8,9 +10,22 @@ function Login() {
   const navigate = useNavigate();
 
   const handleLogin = async (e) => {
-
+      e.preventDefault();
+    try{
+      const res = await loginUser(email, password);
+      const user = res.data.user;
+      localStorage.setItem("user", JSON.stringify(user));
+      navigate("/");
+    }catch (err){
+           alert("error al iniciar sesion");
+      console.log(err);
+    }
   };
 
+  const handleGoogleLogin = async() => {
+    const {error} = await supabase.auth.signInWithOAuth({provider: "google"});
+    if (error) console.log(error);
+  }
   return (
     <div className="container mt-5 col-md-4">
       <h3 className="mb-3">Iniciar sesión</h3>
@@ -19,8 +34,11 @@ function Login() {
         <input type="password" className="form-control mb-2" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)} required />
         <button className="btn btn-primary w-100">Entrar</button>
       </form>
+
       {error && <p className="text-danger mt-2">{error}</p>}
       <p className="mt-3 text-center">¿No tiene cuenta? <Link to="/registro">Regístrese</Link></p>
+        <button className="btn btn-secondary w-100" onClick={handleGoogleLogin}>Iniciar Sesion con Google</button>
+    
     </div>
   );
 }
---
+++ src/utils/supabaseClient.js
@@ -0,0 +1,5 @@
+// npm install @supabase/supabase-js
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient("https://mvhpdpmskvwfmbwarlii.supabase.co"
+    ,"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlpdnhweHZpeXBjdm9hendjZXB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE5OTk5OTcsImV4cCI6MjA1NzU3NTk5N30.CyST7Gm1-wx9YiJJuYE9XkLRMPsK38gDP0IvsLPhUAY")
